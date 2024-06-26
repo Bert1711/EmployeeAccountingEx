c0e0192eb2e55f61f4b2a8e605d40e08649860d7
Albert Zaroyan <albertzaroyan@yandex.ru>
2024-06-26 12:18:56 +0300

    "initial commit"

---
+++ src/test/java/com/zaroyan/employeeaccountingex/EmployeeAccountingExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zaroyan.employeeaccountingex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeAccountingExApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
